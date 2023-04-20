7ecafcb1c1d13d3d050a54fbfde980399e936045
bosco <boscoguo@hotmail.com>
2023-04-21 05:41:25 +1000

    pagination added

--- src/pages/videos/videosPage.tsx
+++ src/pages/videos/videosPage.tsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { Video } from '../../interfaces/video'
 import VideoItem from '../../components/video-item'
 import Spinner from 'react-bootstrap/Spinner'
+import PaginationComponent from '../../components/pagination'
 
 const VideosPage = () => {
   const [data, setData] = useState<Video[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch('./videos.json')
@@ -13,14 +17,27 @@ const VideosPage = () => {
     }
     fetchData()
   }, [])
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem)
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+  }
   const isFetchedData = !!data.length
   return (
     <>
       {isFetchedData ? (
-        data.map(item => <VideoItem key={item.id} video={item} />)
+        currentItems.map(item => <VideoItem key={item.id} video={item} />)
       ) : (
         <Spinner animation="border" variant="warning" />
       )}
+      <PaginationComponent
+        itemsPerPage={itemsPerPage}
+        totalItems={data.length}
+        paginate={paginate}
+      />
     </>
   )
 }
